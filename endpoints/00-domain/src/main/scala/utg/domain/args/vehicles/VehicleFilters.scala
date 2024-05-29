ef3c9131003d729c640e0e6bdf12f4f055291a58
Maftunbek <Prince777_98@mail.ru>
2024-05-29 15:13:21 +0500

    feat(vehicles-api): added to rest api

--- endpoints/00-domain/src/main/scala/utg/domain/args/vehicles/VehicleFilters.scala
+++ endpoints/00-domain/src/main/scala/utg/domain/args/vehicles/VehicleFilters.scala
@@ -2,10 +2,13 @@ package utg.domain.args.vehicles
 
 import eu.timepit.refined.types.numeric.PosInt
 import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.generic.JsonCodec
+import io.circe.refined._
 
 import utg.RegisteredNumber
 import utg.domain.enums.VehicleType
 
+@JsonCodec
 case class VehicleFilters(
     name: Option[NonEmptyString] = None,
     registeredNumber: Option[RegisteredNumber] = None,
--- endpoints/00-domain/src/main/scala/utg/domain/args/vehicles/VehicleInput.scala
+++ endpoints/00-domain/src/main/scala/utg/domain/args/vehicles/VehicleInput.scala
@@ -2,10 +2,13 @@ package utg.domain.args.vehicles
 
 import eu.timepit.refined.types.all.NonNegDouble
 import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.generic.JsonCodec
+import io.circe.refined._
 
 import utg.RegisteredNumber
 import utg.domain.enums.VehicleType
 
+@JsonCodec
 case class VehicleInput(
     name: NonEmptyString,
     registeredNumber: RegisteredNumber,
