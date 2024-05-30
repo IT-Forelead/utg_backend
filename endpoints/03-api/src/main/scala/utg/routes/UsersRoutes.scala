eb9aa6431dbc1bde475fb1ca362f5fc97321a492
Maftunbek <Prince777_98@mail.ru>
2024-05-30 19:07:43 +0500

    feat(user-roles): get user roles

--- endpoints/03-api/src/main/scala/utg/routes/UsersRoutes.scala
+++ endpoints/03-api/src/main/scala/utg/routes/UsersRoutes.scala
@@ -9,6 +9,7 @@ import uz.scala.http4s.syntax.all.deriveEntityEncoder
 import uz.scala.http4s.syntax.all.http4SyntaxReqOps
 import uz.scala.http4s.utils.Routes
 
+import utg.algebras.RolesAlgebra
 import utg.algebras.UsersAlgebra
 import utg.domain.AuthedUser
 import utg.domain.UserId
@@ -17,7 +18,8 @@ import utg.domain.args.users.UserInput
 import utg.domain.enums.Privilege
 
 final case class UsersRoutes[F[_]: JsonDecoder: MonadThrow](
-    users: UsersAlgebra[F]
+    users: UsersAlgebra[F],
+    roles: RolesAlgebra[F],
   ) extends Routes[F, AuthedUser] {
   override val path = "/users"
 
@@ -26,6 +28,7 @@ final case class UsersRoutes[F[_]: JsonDecoder: MonadThrow](
       ar.req.decodeR[UserInput] { create =>
         users.create(create).flatMap(Created(_))
       }
+
     case GET -> Root / UUIDVar(userId) as user if user.access(Privilege.ViewUsers) =>
       users.findById(userId.coerce[UserId]).flatMap(Ok(_))
 
@@ -33,5 +36,8 @@ final case class UsersRoutes[F[_]: JsonDecoder: MonadThrow](
       ar.req.decodeR[UserFilters] { create =>
         users.get(create).flatMap(Ok(_))
       }
+
+    case GET -> Root / "roles" as user if user.access(Privilege.ViewUsers) =>
+      roles.getAll.flatMap(Ok(_))
   }
 }
