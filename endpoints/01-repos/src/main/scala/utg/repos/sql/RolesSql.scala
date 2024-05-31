01606ebc0754ef0732b3c4bae2744abe93dc2def
Maftunbek <Prince777_98@mail.ru>
2024-05-31 11:37:07 +0500

    fix(user-roles): get user roles

--- endpoints/01-repos/src/main/scala/utg/repos/sql/RolesSql.scala
+++ endpoints/01-repos/src/main/scala/utg/repos/sql/RolesSql.scala
@@ -10,17 +10,17 @@ import utg.domain.enums.Privilege
 private[repos] object RolesSql extends Sql[RoleId] {
   private[repos] val codec = (id *: nes).to[dto.Role]
 
-  val getById: Query[RoleId, NonEmptyString *: Privilege *: EmptyTuple] =
-    sql"""SELECT r.name, rp.privilege FROM role_privileges rp
-          join roles r on r.id = rp.role_id
-          WHERE role_id = $id""".query(nes *: privilege)
+  val getById: Query[RoleId, NonEmptyString *: Option[Privilege] *: EmptyTuple] =
+    sql"""SELECT r.name, rp.privilege FROM roles r
+          LEFT JOIN role_privileges rp on r.id = rp.role_id
+          WHERE role_id = $id""".query(nes *: privilege.opt)
 
   def getByIds(
       ids: List[RoleId]
-    ): Query[ids.type, RoleId *: NonEmptyString *: Privilege *: EmptyTuple] =
-    sql"""SELECT r.id, r.name, rp.privilege FROM role_privileges rp
-          join roles r on r.id = rp.role_id
-          WHERE role_id IN (${id.values.list(ids)})""".query(id *: nes *: privilege)
+    ): Query[ids.type, RoleId *: NonEmptyString *: Option[Privilege] *: EmptyTuple] =
+    sql"""SELECT r.id, r.name, rp.privilege FROM roles r
+          LEFT JOIN role_privileges rp on r.id = rp.role_id
+          WHERE role_id IN (${id.values.list(ids)})""".query(id *: nes *: privilege.opt)
 
   val getAll: Query[Void, dto.Role] =
     sql"""
